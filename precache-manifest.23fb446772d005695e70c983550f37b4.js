36465d75c76fb3733232bccc647926ef9611bbeb
Harsh Vasoya <harshvasoya008@gmail.com>
2020-04-26 21:25:47 +0530

    Updates

---
+++ precache-manifest.23fb446772d005695e70c983550f37b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5d1c590075b7ca7c10d2ce884d1531",
+    "url": "/codenames/index.html"
+  },
+  {
+    "revision": "669a8dc01020d11143af",
+    "url": "/codenames/static/css/main.f5c23585.chunk.css"
+  },
+  {
+    "revision": "83d07f7db12da76cc44f",
+    "url": "/codenames/static/js/2.10d84316.chunk.js"
+  },
+  {
+    "revision": "8ad192d0af438cbd7ec1209bbeea6fd9",
+    "url": "/codenames/static/js/2.10d84316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669a8dc01020d11143af",
+    "url": "/codenames/static/js/main.5565de5d.chunk.js"
+  },
+  {
+    "revision": "d68a601de557aed90d45",
+    "url": "/codenames/static/js/runtime-main.1ddca414.js"
+  }
+]);
